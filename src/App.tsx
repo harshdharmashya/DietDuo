310e3dcb49f8aac43fd058038848880a87d4440f
Harsh <157235100+harshdharmashya@users.noreply.github.com>
2024-07-09 16:06:13 +0530

    DietDuo

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState,useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Header from './component/Header'
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      offset: 320, // offset (in px) from the original trigger point
+            delay: 0, // values from 0 to 3000, with step 50ms
+            duration: 2000,}
+    );
+    AOS.refresh();
+  }, []);
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Navbar from "./Navbar"
+import "../CSS/base.css"
+import "../CSS/Header.css"
+import img1 from "../Images/Veg bg.jpg";
+import img2 from "../Images/Allu roti veg meal.jpg";
+import img3 from "../Images/planmeal-weekly-menu-planning-removebg-preview.png"
+
+export default function Header() {
+  return (
+    <>
+    <header className="background-Color header">
+    <Navbar />
+    <div className='first-prv'>
+        <div className='header-img-left'>
+            <img className='img1' src={img1} alt=""/>
+            <img className="img2" src={img2} alt=""/>
+        </div>
+        <div className='header-tag'>
+          <h1  data-aos="fade-down">𝓓𝓲𝓮𝓽𝓓𝓾𝓸...</h1>
+          <div className='header-2ndtext-image-resp'>
+          <div className='seconds'>𝑮𝒆𝒏𝒆𝒓𝒂𝒕𝒆 𝒀𝒐𝒖𝒓 <br /> 𝑪𝒖𝒔𝒕𝒐𝒎 𝑫𝒊𝒆𝒕 <br />𝒊𝒏 𝑺𝒆𝒄𝒐𝒏𝒅𝒔 <br />....</div>
+          <img className='img3'  data-aos="fade-down" src={img3} alt="" />
+          </div>
+        </div>
+      </div>
+      <div>
+          <button className='btn1'>Try ----</button>
+      </div>
+    </header>
+    </>
+  )
+}
